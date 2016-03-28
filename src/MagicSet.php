a15e4a4253a8a47ef0ed960fe7421f3a5866a06e
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2016-03-28 10:47:17 +0200

    add magic setter trait
    Resolves #1

---
+++ src/MagicSet.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Magic Set Trait
+ *
+ * The Magic Set trait provides only the magic '__set' method, to simplify and
+ * unify setting of values to protected properties.
+ *
+ * @package   SlaxWeb\GetSet
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.1
+ */
+namespace SlaxWeb\GetSet;
+
+trait MagicSet
+{
+    /**
+     * Set magic method
+     *
+     * Used to set protected class properties, and ensures that 'name' and
+     * 'definition' properties can not be overwritten.
+     *
+     * @param string $param Name of the protected parameter, without the
+     *                      underscore.
+     * @param mixed $value Value of the property
+     * @exception \SlaxWeb\Exception\UnknownPropertyException Thrown if access
+     *                                                        to unknown
+     *                                                        property is made.
+     * @return void
+     */
+    public function __set(string $param, $value)
+    {
+        $prepend = $this->_getSetPrepend ?? "_";
+        $property = "{$prepend}{$param}";
+        if (isset($this->{$property}) === false) {
+            throw new \SlaxWeb\Exception\UnknownPropertyException(
+                "Property '{$param}' does not exist in " . __CLASS__ . ", "
+                . "unable to get value."
+            );
+        }
+    }
+}
